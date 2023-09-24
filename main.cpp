d10164083c8057486a073fadb25a5044ef59f4c3
ALIAKSEI SYCHOU <144005377+Edlws@users.noreply.github.com>
2023-09-24 15:51:46 +0300

    Array.lab v1.1

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main() {
-    int elrmAmnt, min, max, tmp, k, sum, decider, b, g;
+    int elemAmnt, min, max, k, decider, b, g, h, c;
+    double sum, tmp, hophey;
     const int MAX_SIZE = 100;
     cout << "Enter the number of array elements:"<< endl;
-    cin >> elrmAmnt;
-    if (elrmAmnt <= 0 || elrmAmnt > MAX_SIZE) {
+    cin >> elemAmnt;
+    if (elemAmnt <= 0 || elemAmnt > MAX_SIZE) {
         cout << "Incorrect array size. Please enter a number from 1 to " << MAX_SIZE << endl;
         return 1;
     } else {
@@ -17,8 +18,8 @@ int main() {
             cin >> min;
             cout << "Enter a upper bound for array element values:" << endl; //min!=max & min<(max-min)
             cin >> max;
-            int *dynArray = new int[elrmAmnt];
-            for (int i = 0; i < elrmAmnt; i++) {
+            int *dynArray = new int[2*elemAmnt];
+            for (int i = 0; i < elemAmnt; i++) {
                 dynArray[i] = rand();
                 if (dynArray[i] > max) {
                     dynArray[i] = dynArray[i] % max;
@@ -27,89 +28,91 @@ int main() {
                     dynArray[i] += min;
                 }
             }
-            for (int i = 0; i < elrmAmnt; ++i) {
+            for (int i = 0; i < elemAmnt; ++i) {
                 cout << dynArray[i] << " ";
             }
             cout << endl;
-            tmp = abs(dynArray[0]);
-            for (int i = 0; i < elrmAmnt; i+=1) {
-                if (tmp>abs(dynArray[i])) {
+            tmp = dynArray[0];
+            for (int i = 0; i < elemAmnt; i+=1) {
+                if (tmp>dynArray[i] & tmp > -1* dynArray[i]) {
                     tmp=dynArray[i];
                 }
             }
             sum=0;
-            k= elrmAmnt - 1;
+            k= elemAmnt - 1;
             while (dynArray[k]!=0 & k>=0) {
                 sum+=dynArray[k];
                 k-=1;
             }
-            int tmpArray[elrmAmnt];
-            for(int i=0; i < elrmAmnt; i++){
-                tmpArray[i]=dynArray[i];
+            for(int i=0; i < elemAmnt; i++){
+                dynArray[elemAmnt+i]=dynArray[i];
             }
-            if (elrmAmnt % 2 == 0){
-                b= (elrmAmnt) / 2 - 1;
+            if (elemAmnt % 2 == 0){
+                b= (elemAmnt) / 2 - 1;
             }
             else
             {
-                b= (elrmAmnt) / 2;
+                b= (elemAmnt) / 2;
             }
             for(int i=1; i<b+1; i++){
-                dynArray[i]=tmpArray[i*2];
+                dynArray[i]=dynArray[i*2];
             }
             g=1;
-            for(int i=b+1; i < elrmAmnt; i++){
-                dynArray[i]=tmpArray[g];
+            for(int i=b+1; i < elemAmnt; i++){
+                dynArray[i]=dynArray[elemAmnt+g];
                 g+=2;
             }
             cout << "The sum of the array elements located after the last zero element: " << sum << endl;
             cout << "Minimum modulo array element: " << tmp << endl;
             cout << "New array: " ;
-            for (int i = 0; i < elrmAmnt; ++i) {
+            for (int i = 0; i < elemAmnt; ++i) {
                 cout << dynArray[i] << " ";
             }
         }else{ cout << "Enter the values of the array elements: " << endl;
-            int Array[elrmAmnt];
-            for (int i = 0; i < elrmAmnt; i++){
+            double Array[elemAmnt*2];
+            for (int i = 0; i < elemAmnt; i++){
                 cin >> Array[i];
             }
-            for (int i = 0; i < elrmAmnt; ++i) {
+            for (int i = 0; i < elemAmnt; ++i) {
                 cout << Array[i] << " ";
             }
             cout << endl;
             tmp = abs(Array[0]);
-            for (int i = 0; i < elrmAmnt; i++) {
-                if (tmp>abs(Array[i])) {
+            for (int i = 0; i < elemAmnt; i++) {
+                if (tmp>Array[i] & tmp>Array[i]*(-1)) {
                     tmp=Array[i];
                 }
             }
             sum=0;
-            k= elrmAmnt - 1;
+            k= elemAmnt - 1;
             while (Array[k]!=0 & k>=0) {
                 sum+=Array[k];
                 k-=1;
             }
-            int tmpArray[elrmAmnt];
-            for(int i=0; i < elrmAmnt; i++){
-                tmpArray[i]=Array[i];
+            for(int i=0; i < elemAmnt; i++){
+                Array[elemAmnt+i]=Array[i];
             }
-            if (elrmAmnt % 2 == 0){
-                b= (elrmAmnt) / 2 - 1;
-            } else{
-                b= (elrmAmnt) / 2;
+            if (elemAmnt % 2 == 0){
+                b= (elemAmnt) / 2 - 1;
+            }
+            else
+            {
+                b= (elemAmnt) / 2;
             }
             for(int i=1; i<b+1; i++){
-                Array[i]=tmpArray[i*2];
+                Array[i]=Array[i*2];
             }
             g=1;
-            for(int i=b+1; i < elrmAmnt; i++){
-                Array[i]=tmpArray[g];
+            for(int i=b+1; i < elemAmnt; i++){
+                Array[i]=Array[elemAmnt+g];
                 g+=2;
             }
+
+
             cout << "The sum of the array elements located after the last zero element: " << sum << endl;
             cout << "Minimum modulo array element: " << tmp << endl;
             cout << "New array: " ;
-            for (int i = 0; i < elrmAmnt; ++i) {
+            for (int i = 0; i < elemAmnt; i+=1) {
                 cout << Array[i] << " ";
             }
         }
